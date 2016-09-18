70b002ca329aa1843459837d695a884240fe64cd
Giuseppe de Marino <giuseppe.demarino@bbc.co.uk>
2016-09-18 14:37:49 +0100

    added test suite.

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -6,7 +6,6 @@
    * @constructor
    * @this {CustomerBill}
    */
-
     function CustomerBill() {
       var _this = this;
 
@@ -19,16 +18,20 @@
 
       //call the addEvent method and check element it is not null or undefined
       if(this.billView){
-        this.billView.addEventListener( 'click', this.getQuery.bind(_this) );
+        this.billView.addEventListener( 'click', function(){
+          _this.getQuery(_this.dataSuccess.bind(_this));
+        });
       }
 
     }
 
     /**
      * getQuery make the ajax call to the feed.
+     * @param {[Function]} callback
      */
-    CustomerBill.prototype.getQuery = function () {
-      var _this = this;
+    CustomerBill.prototype.getQuery = function ( callback ) {
+      var _this = this,
+          deferred = $.Deferred();
 
         if(_this.bill){
           _this.bill.classList.add('spinner');
@@ -40,41 +43,61 @@
             url: _this.queryEndPoint,
             cache: false,
             dataType: 'json',
-            success: function ( data ) {
-              _this.dataSuccess(data);
-              _this.bill.classList.remove('spinner');
-            },
-            error: function ( error ) {
-              _this.error.innerHTML = error;
-            }
+            success: onResponse,
+            error: onError
         });
+
+        /**
+         * Return a successful promise
+         * @param {[Object]} response
+         */
+        function onResponse(response) {
+            deferred.resolve(response);
+            callback(response);
+        }
+
+        /**
+         * Return an error on rejection
+         * @param {[Object]} response
+         */
+        function onError(response) {
+            deferred.reject(response);
+            callback((response.status +' '+ response.statusText));
+        }
+
+        return deferred.promise();
     };
 
     /**
      * dataLoop is an helper function that loop over the data returned.
-     * @param  {[type]}   arr          Array
-     * @param  {[type]}   callBackArg   String
-     * @param  {[type]}   newArr        Array
+     * @param  {[Array]}   arr
+     * @param  {[Any]}   callBackArg
+     * @param  {[Array]}   newArr
      */
     CustomerBill.prototype.dataLoop = function ( arr, callBackArg ,newArr ) {
-      //safe check to make sure first argument it is an array.
-      if(arr){
+      //safe check to make sure all arguments are passed in, and they are
+      //of the right type.
+      if($.isArray(arr) &&
+        callBackArg !== null &&
+        $.isArray(newArr)
+      ){
         arr.forEach( function( callBackArg ) {
           newArr.push(callBackArg);
         });
+      } else{
+        throw new Error('Missing argument or argument is of the wrong type.');
       }
-
     };
 
     /**
      * dataSuccess deal with the data and pass it back to handlebars view.
-     * @param  {[type]}   data          Object
+     * @param  {[Object]}   data
      */
     CustomerBill.prototype.dataSuccess = function ( data ) {
 
       //lets check we have some data back, and the data is the right format before we proceed.
       if(!data && data !=='object'){
-        throw new Error('missing data');
+        throw new Error('Missing data');
       }
 
       var _this = this,
@@ -88,7 +111,8 @@
           skyStoreTotalBuyAndKeep = [],
           template,
           context,
-          html;
+          html,
+          str ='';
 
       //lets remove the spinner once we have data succesfully back.
       if(_this.bill){
@@ -96,12 +120,10 @@
       }
 
       //calling the dataLoop helper function to work with our data set
-      if(data){
-        _this.dataLoop( data.callCharges.calls, call, callTotal);
-        _this.dataLoop( data.package.subscriptions, subscription, packagesTotal);
-        _this.dataLoop( data.skyStore.rentals, rental, skyStoreTotalRentals);
-        _this.dataLoop( data.skyStore.buyAndKeep, buy, skyStoreTotalBuyAndKeep);
-      }
+      _this.dataLoop( data.callCharges.calls, call, callTotal);
+      _this.dataLoop( data.package.subscriptions, subscription, packagesTotal);
+      _this.dataLoop( data.skyStore.rentals, rental, skyStoreTotalRentals);
+      _this.dataLoop( data.skyStore.buyAndKeep, buy, skyStoreTotalBuyAndKeep);
 
       //Handlebars to update the view with the right data.
       template = Handlebars.compile( $( '#output-results' ).html() );
--- test/mainTest.js
+++ test/mainTest.js
@@ -1,68 +1,116 @@
 var expect = chai.expect;
 var assert = chai.assert;
+var should = chai.should();
+
 
 describe("Customer Bill Tests", function() {
-  var bill = new CustomerBill();
-  var mock = {
-    statement:{
-      period:{
-        from:'',
-        to:''
-      }
-    },
-    package: {
-        subscriptions: [{
-        type: "tv",
-        name: "Variety with Movies HD",
-        cost: 50
-      }]
-    },
-    callCharges: {
-      calls: [
-        {
-          called: "07716393769",
-          duration: "00:23:03",
-          cost: 2.13
+  var bill = new CustomerBill(),
+      data = {
+        statement:{
+          period:{
+            from:'',
+            to:''
+          }
+        },
+        package: {
+            subscriptions: [{
+            type: "tv",
+            name: "Variety with Movies HD",
+            cost: 50
+          }]
+        },
+        callCharges: {
+          calls: [
+            {
+              called: "07716393769",
+              duration: "00:23:03",
+              cost: 2.13
+            }
+          ]
+        },
+        skyStore: {
+          rentals: [
+            {
+              title: "50 Shades of Grey",
+              cost: 4.99
+            }
+          ]
         }
-      ]
-    },
-    skyStore: {
-      rentals: [
-        {
-          title: "50 Shades of Grey",
-          cost: 4.99
-        }
-      ],
-      buyAndKeep: [
-        {
-          title: "That's what she said",
-          cost: 9.99
-        }
-      ]
-    }
-  }
-  describe("CustomerBill constructor.", function() {
-    it("Object should exist when initialized.", function() {
-      var spy = chai.spy(bill);
-      expect(spy).to.be.called
+      };
+
+    describe("CustomerBill constructor.", function() {
+      it("Object should exist when initialized.", function() {
+        var spy = sinon.spy(window, 'CustomerBill');
+        var bill = new CustomerBill();
+
+        expect(spy.called).to.be.equal(true);
+      });
     });
+
+    describe('getQuery method.', function() {
+      //no to make a real call, fake server
+      //create a response to work with.
+        beforeEach(function() {
+            this.xhr = sinon.useFakeXMLHttpRequest();
+
+            this.requests = [];
+            this.xhr.onCreate = function(xhr) {
+                this.requests.push(xhr);
+            }.bind(this);
+        });
+
+        afterEach(function() {
+            this.xhr.restore();
+        });
+
+        it('should return a successful response on a 200 status code.', function(done) {
+          //Parse the response to json object first.
+          var dataJson = JSON.stringify(data);
+
+          bill.getQuery(function(result) {
+              result.should.deep.equal(data);
+              done();
+          });
+
+          this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
+        });
+
+        it('should not return data on a 400 Bad Request.', function(done) {
+          bill.getQuery(function(result) {
+              result.should.not.to.be.deep.equal(data);
+              done();
+          });
+
+          this.requests[0].respond(400);
+        });
+
+        it('should return an error on a 500 Internal Server Error.', function(done) {
+            bill.getQuery(function(err) {
+                err.should.exist;
+                done();
+            });
+
+            this.requests[0].respond(500);
+        });
+
+      });
+
+      describe('dataLoop method.', function() {
+        it('should throw an error if called with no arguments.', function(done) {
+          var spy = chai.spy(bill, 'dataLoop');
+
+          expect(spy).to.throw(Error);
+          done();
+        });
+
+        it('should be called with right arguments.', function(done) {
+          var spy = sinon.spy(bill, 'dataLoop'),
+              arr = [],
+              el = "";
+          spy(arr,el,arr);
+
+          spy.firstCall.args;
+          done();
+        });
+      });
   });
-
-
-  describe("dataLoop.", function() {
-    it("should be called with all 3 parameters.", function() {
-      var spy = chai.spy.on(bill,'dataLoop');
-      var arr,callBackArg,newArr;
-       expect(spy).to.have.been.called.with(arr);
-    });
-  });
-
-  describe("dataSuccess.", function() {
-    it("should throw an error if no argument has been passed.", function() {
-      expect(function() {
-       (bill).dataSuccess();
-     }).to.throw(Error);
-    });
-  });
-
-});
